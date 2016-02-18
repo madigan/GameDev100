092c957bde23bb0e2e8f407ceb7d322b15c8cf2c
John Lynn <john@silferein.com>
2016-02-17 21:24:33 -0800

    Add keyboard example.

---
+++ keyboard_example.rb
@@ -0,0 +1,39 @@
+require 'gosu'
+
+class KeyboardExample < Gosu::Window
+	def initialize
+		super 640, 480
+		self.caption = "Keyboard Example"
+		@font = Gosu::Font.new(32, name: "Nimbus Mono L")
+		input = Gosu::TextInput.new
+		def input.filter text_in
+		    puts 'I got called!'
+		    'Otter!'
+		end
+		self.text_input = input
+		self.text_input.text = "Type something!"
+		
+		@text_y = 20
+	end
+	
+	def button_up(key_id)
+	    if key_id == Gosu::KbEscape then
+	        close
+	    end
+	end
+	
+	def update
+	    if button_down?(Gosu::KbDown) then
+	        @text_y += 1
+	    elsif button_down?(Gosu::KbUp) then
+	        @text_y -= 1
+	    end
+	end
+
+	def draw
+	    @font.draw(self.text_input.text, 20, @text_y, 0)
+	end
+end
+
+window = KeyboardExample.new
+window.show
